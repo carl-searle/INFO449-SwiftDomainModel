5e595722debfa72613e1b44a09f3ee95fb154fff
carl-searle <carl1298@outlook.com>
2024-04-12 23:35:36 -0700

    committed most work

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -9,6 +9,51 @@ struct DomainModel {
 // Money
 //
 public struct Money {
+    let amount: Int
+    let currency: String
+
+    init(amount: Int, currency: String) {
+        self.amount = amount
+        self.currency = currency
+    }
+
+    func convert(_ givenCurrency: String) -> Money {
+        var usdConvert = amount
+        // need to figure out rounding
+        // converts to usd first, if already usd does not need to be converted at beginning
+        if(currency == "GBP") {
+            usdConvert *= 2
+        } else if(currency == "EUR") {
+            usdConvert *= 2
+            usdConvert /= 3
+        } else if(currency == "CAN") {
+            usdConvert *= 4
+            usdConvert /= 5
+        }
+        if(givenCurrency == "GBP") {
+            usdConvert /= 2
+        } else if(givenCurrency == "EUR") {
+            usdConvert *= 3
+            usdConvert /= 2
+        } else if(givenCurrency == "CAN") {
+            usdConvert *= 5
+            usdConvert /= 4
+        }
+        return Money(amount: usdConvert, currency: givenCurrency)
+    }
+
+    // in the tests, the money that is passed in the parameter is the currency we need to convert to
+    func add(_ secondMoney: Money) -> Money {
+        let convertMoney = self.convert(secondMoney.currency)
+        let totalMoney = convertMoney.amount + secondMoney.amount
+        return Money(amount: totalMoney, currency: secondMoney.currency)
+    }
+
+    func subtract(_ secondMoney: Money) -> Money {
+        let convertMoney = self.convert(secondMoney.currency)
+        let totalMoney = convertMoney.amount - secondMoney.amount
+        return Money(amount: totalMoney, currency: secondMoney.currency)
+    }
 }
 
 ////////////////////////////////////
@@ -19,12 +64,107 @@ public class Job {
         case Hourly(Double)
         case Salary(UInt)
     }
+    // hourly is double
+    // salary is int
+
+    let title: String
+    var type: JobType
+
+    init(title: String, type: JobType) {
+        self.title = title
+        self.type = type
+    }
+
+    // returns amount of money as int, that in one year this position makes
+    // for salary, this is yearly amount, for hourly, this is * given hours worked
+    func calculateIncome(_ hoursWorked: Int) -> Int {
+        switch type {
+            case .Hourly(let hourlyWage):
+                return Int(hourlyWage) * hoursWorked
+            case .Salary(let salary):
+                return Int(salary)
+        }
+    }
+
+    func raise(byAmount: Double) {
+        // by hourly or salary (use switch and case)
+        // calculate what the value would be with calculateIncome
+        // assign the value to type
+        switch type {
+            case .Hourly(let hourlyWage):
+                let calculatedWage = hourlyWage + byAmount
+                type = JobType.Hourly(calculatedWage)
+            case .Salary(let salary):
+                let calculatedWage = Double(salary) + byAmount
+                type = JobType.Salary(UInt(calculatedWage))
+        }
+    }
+
+    func raise(byPercent: Double) {
+        // by hourly or salary (use switch and case)
+        // calculate what the value would be with calculateIncome
+        // assign the value to type
+        switch type {
+            // to calculate: (amount * bypercent) + amount
+            case .Hourly(let hourlyWage):
+                let calculatedWage = hourlyWage * byPercent + hourlyWage
+                type = JobType.Hourly(calculatedWage)
+            case .Salary(let salary):
+                let calculatedWage = Double(salary) * byPercent + Double(salary)
+                type = JobType.Salary(UInt(calculatedWage))
+        }
+    }
 }
 
 ////////////////////////////////////
 // Person
 //
 public class Person {
+    let firstName: String
+    let lastName: String
+    let age: Int
+    // since these are not always present
+    var job: Job?
+    var spouse: Person?
+
+    init(firstName: String, lastName: String, age: Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+    }
+
+    // get and set spouse, i assumed restriction was 16 as well
+    var _spouse: Person? {
+        get {
+            return spouse
+        } 
+        set {
+            if age >= 16 {
+                // why am i not able to change the name of newValue? ask ta
+                spouse = newValue
+            } else {
+                spouse = nil
+            }
+        }
+    }
+
+    // get and set job, restriction of 16
+    var _job: Job? {
+        get {
+            return job
+        }
+        set {
+            if age >= 16 {
+                job = newValue
+            } else {
+                job = nil
+            }
+        }
+    }
+
+    func toString() -> String {
+        return "[Person: firstName:\(firstName) lastName:\(lastName) age:\(age) job:\(String(describing: job)) spouse:\(String(describing: spouse))]"
+    }
 }
 
 ////////////////////////////////////
